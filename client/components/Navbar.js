f7865243349ebdfc1d86f8ff1107bc0d5708a47d
Garrison Tyson <garrison.r.tyson@gmail.com>
2021-08-03 14:25:41 -0700

    navbar rendering & started building allFood component

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,64 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../store";
+// import "../../public/styles/nav.css";
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>FS-App-Template</h1>
+const Navbar = ({ handleClick, isLoggedIn }) => (
+  <div id="navbar">
     <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+      <div id="navleft">
+        <span id="logo">Hyrule Shopper</span>
+      </div>
+      <div id="navRight">
+        {isLoggedIn ? (
+          <ul>
+            {/* The navbar will show these links after you log in */}
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </li>
+            <li>Cart</li>
+          </ul>
+        ) : (
+          <ul>
+            {/* The navbar will show these links before you log in */}
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li><a href="">Cart</a></li>
+          </ul>
+        )}
+      </div>
     </nav>
-    <hr />
   </div>
-)
+);
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    isLoggedIn: !!state.auth.id
-  }
-}
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleClick() {
-      dispatch(logout())
-    }
-  }
-}
+      dispatch(logout());
+    },
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
---
+++ client/components/allFoods/AllFood.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const fakeData = [
+  {
+    id: 1,
+    price: 9.99,
+    image:
+      "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/9/9d/BotW_Hasty_Elixir_Icon.png/revision/latest/scale-to-width-down/178?cb=20210704015804",
+  },
+];
+
+class AllFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        sdlfoisef jaehfropiasdg opisfpoi sfio hsiodfgh iosehfo asHFGOs
+        ofasehoiugh iouherg hi hi hi hi hi hi
+      </div>
+    );
+  }
+}
+
+export default AllFood;
