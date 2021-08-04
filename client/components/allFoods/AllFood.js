7ee574cec1162dbdfaae2107c014f069015059a8
John Hagerman <jthagerma@gmail.com>
2021-08-03 22:38:45 -0400

    worked on allproducts view and style

--- client/components/allFoods/AllFood.js
+++ client/components/allFoods/AllFood.js
@@ -1,13 +1,81 @@
 import React from "react";
 import { connect } from "react-redux";
+import AllFoodItem from "./AllFoodItem";
 
 const fakeData = [
   {
     id: 1,
-    price: 9.99,
-    image:
-      "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/9/9d/BotW_Hasty_Elixir_Icon.png/revision/latest/scale-to-width-down/178?cb=20210704015804",
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+    "https://cdn.mos.cms.futurecdn.net/8HR4FLWRv8DqYQqXoQnhB8.jpg",
+
   },
+  {
+    id: 2,
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
+
+  },
+  {
+    id: 3,
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
+
+  },
+  {
+    id: 4,
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
+
+  },
+  {
+    id: 5,
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
+
+  },
+  {
+    id: 6,
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
+
+  },
+  {
+    id: 7,
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
+
+  },
+  {
+    id: 8,
+    name: 'Big Hearty Radish',
+    cost: 10,
+    imageUrl:
+        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
+
+  },
+  {
+    id: 9,
+    cost: 9,
+    name: "red food",
+    imageUrl:
+      "https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=h30EAnkk",
+
+  },
+
 ];
 
 class AllFood extends React.Component {
@@ -16,9 +84,21 @@ class AllFood extends React.Component {
   }
   render() {
     return (
-      <div>
-        sdlfoisef jaehfropiasdg opisfpoi sfio hsiodfgh iosehfo asHFGOs
-        ofasehoiugh iouherg hi hi hi hi hi hi
+      <div id="allProductsContainer">
+        <div id="allProuctsHolder">
+        {
+          fakeData.map((element) => {
+            return <AllFoodItem
+            name={element.name}
+            key={element.id}
+            cost={element.cost}
+            imageUrl={element.imageUrl} />
+          })
+        }
+
+        </div>
+
+
       </div>
     );
   }
