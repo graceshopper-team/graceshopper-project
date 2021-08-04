7cfecd133f41c3fcfd6ad801b9eddc5915369444
John Hagerman <jthagerma@gmail.com>
2021-08-03 22:41:34 -0400

    ran linter

--- client/components/allFoods/AllFood.js
+++ client/components/allFoods/AllFood.js
@@ -5,67 +5,58 @@ import AllFoodItem from "./AllFoodItem";
 const fakeData = [
   {
     id: 1,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
-    imageUrl:
-    "https://cdn.mos.cms.futurecdn.net/8HR4FLWRv8DqYQqXoQnhB8.jpg",
-
+    imageUrl: "https://cdn.mos.cms.futurecdn.net/8HR4FLWRv8DqYQqXoQnhB8.jpg",
   },
   {
     id: 2,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
     imageUrl:
-        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
-
+      "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
   },
   {
     id: 3,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
     imageUrl:
-        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
-
+      "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
   },
   {
     id: 4,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
     imageUrl:
-        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
-
+      "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
   },
   {
     id: 5,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
     imageUrl:
-        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
-
+      "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
   },
   {
     id: 6,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
     imageUrl:
-        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
-
+      "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
   },
   {
     id: 7,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
     imageUrl:
-        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
-
+      "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
   },
   {
     id: 8,
-    name: 'Big Hearty Radish',
+    name: "Big Hearty Radish",
     cost: 10,
     imageUrl:
-        "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
-
+      "https://static.wikia.nocookie.net/zelda/images/f/f1/Breath_of_the_Wild_Vegetables_%28Radishes%29_Big_Hearty_Radish_%28Icon%29.png/revision/latest?cb=20170808072417",
   },
   {
     id: 9,
@@ -73,9 +64,7 @@ const fakeData = [
     name: "red food",
     imageUrl:
       "https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=h30EAnkk",
-
   },
-
 ];
 
 class AllFood extends React.Component {
@@ -86,19 +75,17 @@ class AllFood extends React.Component {
     return (
       <div id="allProductsContainer">
         <div id="allProuctsHolder">
-        {
-          fakeData.map((element) => {
-            return <AllFoodItem
-            name={element.name}
-            key={element.id}
-            cost={element.cost}
-            imageUrl={element.imageUrl} />
-          })
-        }
-
+          {fakeData.map((element) => {
+            return (
+              <AllFoodItem
+                name={element.name}
+                key={element.id}
+                cost={element.cost}
+                imageUrl={element.imageUrl}
+              />
+            );
+          })}
         </div>
-
-
       </div>
     );
   }
